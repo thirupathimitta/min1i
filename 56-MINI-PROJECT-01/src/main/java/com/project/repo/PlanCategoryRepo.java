dc4d55dcf606994f7b96fdd57f273db7b11d5d20
thirupathimitta <125956474+thirupathimitta@users.noreply.github.com>
2023-12-07 11:49:28 +0530

    Delete 56-MINI-PROJECT-01 directory

--- 56-MINI-PROJECT-01/src/main/java/com/project/repo/PlanCategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.project.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.entity.PlanCategoryEntity;
-
-public interface PlanCategoryRepo extends JpaRepository<PlanCategoryEntity, Integer>{
-
-}
